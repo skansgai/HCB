bc3c8ad2ff3ea5966542789eea0afb27176338b4
Ranhaihan <594576783@qq.com>
2016-12-12 18:13:00 +0800

    关注  搜索  推荐

---
+++ HaoChiBao/app/src/main/java/com/haochibao/activity/SearchActivity.java
@@ -0,0 +1,17 @@
+package com.haochibao.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.haochibao.R;
+
+/**
+ * Created by Administrator on 2016/12/12.
+ */
+public class SearchActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
---
+++ HaoChiBao/app/src/main/java/com/haochibao/utill/adapter/TuijianListAdapter.java
@@ -0,0 +1,38 @@
+package com.haochibao.utill.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import com.haochibao.R;
+
+/**
+ * Created by Administrator on 2016/12/12.
+ */
+public class TuijianListAdapter extends BaseAdapter {
+    Context context;
+    public TuijianListAdapter(Context context){this.context=context;}
+    @Override
+    public int getCount() {
+        return 5;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater layoutInflater=LayoutInflater.from(context);
+        convertView=layoutInflater.inflate(R.layout.tuijian_listview,null);
+        return convertView;
+    }
+}
