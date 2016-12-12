2ca5072894792766e1519fdc9b0fa78ce5f2e3af
luodawei <1078926400.qq.com>
2016-12-12 13:40:10 +0800

    流式布局

--- HaoChiBao/app/src/main/java/com/haochibao/activity/CommentActivity.java
+++ HaoChiBao/app/src/main/java/com/haochibao/activity/CommentActivity.java
@@ -2,20 +2,85 @@ package com.haochibao.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import com.haochibao.R;
 import com.haochibao.utill.adapter.CommentListAdater;
+import com.haochibao.utill.view.FlowLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Administrator on 2016/12/10.
  */
 public class CommentActivity extends Activity {
+    RadioGroup rgTop;
+    RadioButton rgTopLeft;
+    RadioButton rgTopRight;
+
+    FlowLayout searchList;
+    String[] searchNames = {"卡密尔","艾翁","艾希","艾瑞莉娅","巨魔",
+            "艾翁","艾希","艾瑞莉娅","巨魔","艾翁","艾希"
+            ,"艾翁","艾希","艾瑞莉娅","巨魔","艾瑞莉娅","巨魔",
+            "艾翁","艾希","艾瑞莉娅","巨魔"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comment);
         ListView commentList = (ListView) findViewById(R.id.comment_list);
+        searchList = (FlowLayout) findViewById(R.id.comment_search_list);
+        rgTop = (RadioGroup) findViewById(R.id.rg_top);
+        rgTopLeft = (RadioButton) findViewById(R.id.rg_top_left);
+        rgTopRight = (RadioButton) findViewById(R.id.rg_top_right);
+        addSearchList();
+        //设置RadioButton的初始状态
+        resetRadioButton(1);
+        rgTopLeft.setChecked(true);
+
         commentList.setAdapter(new CommentListAdater(this));
+        rgTop.setOnCheckedChangeListener(getOnCheckedChangeListener());
+    }
+    public void addSearchList(){
+        LayoutInflater inflater = LayoutInflater.from(this);
+        for (int i=0;i<searchNames.length;i++){
+            TextView hotSearchChild = (TextView) inflater.inflate(R.layout.hot_search_child,searchList,false);
+            hotSearchChild.setText(searchNames[i]);
+            searchList.addView(hotSearchChild);
+        }
+    }
+    public RadioGroup.OnCheckedChangeListener getOnCheckedChangeListener(){
+        RadioGroup.OnCheckedChangeListener onCheckedChangeListener = new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+
+                switch (checkedId){
+                    case R.id.rg_top_left:
+                        resetRadioButton(1);
+                        break;
+                    case R.id.rg_top_right:
+                        resetRadioButton(2);
+                        break;
+                }
+            }
+        };
+        return onCheckedChangeListener;
+    }
+    public void resetRadioButton(int i){
+        switch (i){
+            case 1:
+                rgTopLeft.setTextColor(getResources().getColor(R.color.mainRed));
+                rgTopRight.setTextColor(getResources().getColor(R.color.white));
+                break;
+            case 2:
+                rgTopRight.setTextColor(getResources().getColor(R.color.mainRed));
+                rgTopLeft.setTextColor(getResources().getColor(R.color.white));
+                break;
+        }
     }
 }
