0d63367fc6b2b749f7e8fe15ab7c3ed95e3b2ec3
luodawei <1078926400.qq.com>
2016-12-13 14:24:34 +0800

    准备做引导页

--- HaoChiBao/app/src/main/java/com/haochibao/activity/EntertainmentActivity.java
+++ HaoChiBao/app/src/main/java/com/haochibao/activity/EntertainmentActivity.java
@@ -1,6 +1,7 @@
 package com.haochibao.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
