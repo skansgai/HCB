1601b3d9c44bd5ee74510fe70490b108a770ac1f
josephxxc <749852666@qq.com>
2016-12-13 16:27:30 +0800

    底部导航栏及一些跳转到次级页面

--- HaoChiBao/app/src/main/java/com/haochibao/fragment/HomeFragment.java
+++ HaoChiBao/app/src/main/java/com/haochibao/fragment/HomeFragment.java
@@ -1,6 +1,6 @@
 package com.haochibao.fragment;
 
-import android.content.Context;
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -8,31 +8,90 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.RelativeLayout;
+import android.widget.TextView;
+
 import com.haochibao.R;
-import com.haochibao.activity.MineCollectionActivity;
-import com.haochibao.activity.MineInformationActivity;
-import com.haochibao.activity.Settingactivity;
+import com.haochibao.activity.HotelActivity;
+import com.haochibao.activity.ParkingActivity;
+import com.haochibao.activity.RecommendActivity;
+import com.haochibao.activity.ShopingActivity;
+
 /**
  * Created by Administrator on 2016/12/13.
  * 我的页面
  */
 
 public class HomeFragment extends Fragment {
+    TextView home_recommend,home_interaction,home_seek_help,home_recreation,
+            home_grogshop,home_scenic_spots,home_shopping,home_park;
     private View view;
-    private Context context;
+    private Activity mactivity;
+    Intent intent;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mactivity=getActivity();
     }
-
     @Nullable
     @Override
     public View onCreateView(
             LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view=inflater.inflate(R.layout.activity_homepage,null);
-        context=getActivity();
+
+
+        home_recommend = (TextView) view.findViewById(R.id.home_recommend);
+        home_interaction = (TextView) view.findViewById(R.id.home_interaction);
+        home_seek_help = (TextView) view.findViewById(R.id.home_seek_help);
+        home_recreation = (TextView) view.findViewById(R.id.home_recreation);
+        home_grogshop = (TextView) view.findViewById(R.id.home_grogshop);
+        home_scenic_spots = (TextView) view.findViewById(R.id.home_scenic_spots);
+        home_shopping = (TextView) view.findViewById(R.id.home_shopping);
+        home_park = (TextView) view.findViewById(R.id.home_park);
+
+        home_recommend.setOnClickListener(onClickListener);
+        home_interaction.setOnClickListener(onClickListener);
+        home_seek_help.setOnClickListener(onClickListener);
+        home_recreation.setOnClickListener(onClickListener);
+        home_grogshop.setOnClickListener(onClickListener);
+        home_scenic_spots.setOnClickListener(onClickListener);
+        home_shopping.setOnClickListener(onClickListener);
+        home_park.setOnClickListener(onClickListener);
+
         return view;
     }
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.home_recommend:
+                    intent = new Intent(mactivity,RecommendActivity.class);
+                    startActivity(intent);
+                    break;
+                case R.id.home_interaction:
+
+                    break;
+                case R.id.home_seek_help:
+                    break;
+                case R.id.home_recreation:
+                    break;
+                case R.id.home_grogshop:
+                    intent = new Intent(mactivity,HotelActivity.class);
+                    startActivity(intent);
+                    break;
+                case R.id.home_scenic_spots:
+                    break;
+                case R.id.home_shopping:
+                    intent = new Intent(mactivity,ShopingActivity.class);
+                    startActivity(intent);
+                    break;
+                case R.id.home_park:
+                    intent = new Intent(mactivity,ParkingActivity.class);
+                    startActivity(intent);
+                    break;
+            }
+
+        }
+    };
 
 }
