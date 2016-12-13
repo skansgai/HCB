ba284baf403f17545aac7691de58131cdac45a2e
Ranhaihan <594576783@qq.com>
2016-12-13 12:50:15 +0800

    no message

--- HaoChiBao/app/src/main/java/com/haochibao/activity/FindListview.java
+++ HaoChiBao/app/src/main/java/com/haochibao/activity/FindListview.java
@@ -10,12 +10,12 @@ import com.haochibao.utill.adapter.FindListvityAdapter;
 /**
  * Created by Administrator on 2016/12/13.
  */
-public class Find_listvity extends Activity {
+public class FindListview extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find);
         ListView listView= (ListView) findViewById(R.id.find_list);
-        listView.setAdapter(new FindListvityAdapter(Find_listvity.this));
+        listView.setAdapter(new FindListvityAdapter(FindListview.this));
     }
 }
