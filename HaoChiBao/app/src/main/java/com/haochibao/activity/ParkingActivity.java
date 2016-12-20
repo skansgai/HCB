780ac56c39760585b7092178618b4a77ce5c4a59
josephxxc <749852666@qq.com>
2016-12-20 15:24:51 +0800

    下拉列表spinner

--- HaoChiBao/app/src/main/java/com/haochibao/activity/ParkingActivity.java
+++ HaoChiBao/app/src/main/java/com/haochibao/activity/ParkingActivity.java
@@ -1,25 +1,13 @@
 package com.haochibao.activity;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import com.haochibao.R;
-import com.haochibao.fragment.ParkingContentFragment;
-import com.haochibao.fragment.ParkingGoodReputationFragment;
-import com.haochibao.fragment.ParkingNearbyFragment;
-import com.haochibao.fragment.ParkingRankFragment;
 import com.haochibao.utill.adapter.EntertainmentAdapter;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by Administrator on 2016/12/12.
@@ -27,6 +15,7 @@ import java.util.List;
 public class ParkingActivity extends FragmentActivity {
     ListView parkingList;
     ImageView img_left;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,17 +27,21 @@ public class ParkingActivity extends FragmentActivity {
         img_left.setOnClickListener(getOnClickListener());
         parkingList.setAdapter(new EntertainmentAdapter(this));
     }
-    public View.OnClickListener getOnClickListener(){
+
+    public View.OnClickListener getOnClickListener() {
         View.OnClickListener onClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                switch (v.getId()){
+                switch (v.getId()) {
                     case R.id.img_left:
                         finish();
                         break;
+
                 }
             }
         };
         return onClickListener;
     }
+
+
 }
