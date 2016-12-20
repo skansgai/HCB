a09fdb688d27b6d43ca5e8a58a633c5cd548e91e
skansgai <1026334022@qq.com>
2016-12-20 15:23:00 +0800

    no message

---
+++ HaoChiBao/app/src/main/java/com/haochibao/utill/http/URIBitmap.java
@@ -0,0 +1,18 @@
+package com.haochibao.utill.http;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+
+import java.io.InputStream;
+import java.net.URI;
+
+/**
+ * Created by Administrator on 2016/12/20.
+ */
+
+public class URIBitmap {
+    public static Bitmap getBitmapFormUri(Activity activity, URI uri){
+       // InputStream input=activity.getContentResolver().openInputStream(uri);
+        return null;
+    }
+}
--- HaoChiBao/app/src/main/java/com/haochibao/utill/view_holder/SortViewHolder.java
+++ HaoChiBao/app/src/main/java/com/haochibao/utill/view_holder/SortViewHolder.java
@@ -4,5 +4,5 @@ package com.haochibao.utill.view_holder;
  * Created by Administrator on 2016/12/20.
  */
 public class SortViewHolder {
-    private
+
 }
