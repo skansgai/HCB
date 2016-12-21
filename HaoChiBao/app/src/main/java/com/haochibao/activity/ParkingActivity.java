8f8c3af4330e71f4fc81f9ddb0873ff25e8e35f4
Ranhaihan <594576783@qq.com>
2016-12-21 14:28:44 +0800

    112

--- HaoChiBao/app/src/main/java/com/haochibao/activity/ParkingActivity.java
+++ HaoChiBao/app/src/main/java/com/haochibao/activity/ParkingActivity.java
@@ -8,6 +8,10 @@ import android.widget.ListView;
 
 import com.haochibao.R;
 import com.haochibao.utill.adapter.EntertainmentAdapter;
+import com.haochibao.utill.model.EntertainmentModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Administrator on 2016/12/12.
@@ -15,6 +19,7 @@ import com.haochibao.utill.adapter.EntertainmentAdapter;
 public class ParkingActivity extends FragmentActivity {
     ListView parkingList;
     ImageView img_left;
+    List<EntertainmentModel>list;
 
 
     @Override
@@ -23,9 +28,10 @@ public class ParkingActivity extends FragmentActivity {
         setContentView(R.layout.activity_parking);
         parkingList = (ListView) findViewById(R.id.parking_list);
         img_left = (ImageView) findViewById(R.id.img_left);
+        list = new ArrayList<EntertainmentModel>();
 
         img_left.setOnClickListener(getOnClickListener());
-        parkingList.setAdapter(new EntertainmentAdapter(this));
+        parkingList.setAdapter(new EntertainmentAdapter(this,list));
     }
 
     public View.OnClickListener getOnClickListener() {
