b88f6ca427e50ced881bc38b35a1c4090c2e64cf
josephxxc <749852666@qq.com>
2016-12-22 15:03:42 +0800

    推荐的网络请求

---
+++ HaoChiBao/app/src/main/java/com/haochibao/utill/model/Recommend.java
@@ -0,0 +1,54 @@
+package com.haochibao.utill.model;
+
+/**
+ * Created by Administrator on 2016/12/22.
+ */
+
+public class Recommend {
+
+    private String name;
+    private double grade;
+    private String img;
+    private String describe;
+    private double price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getGrade() {
+        return grade;
+    }
+
+    public void setGrade(double grade) {
+        this.grade = grade;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public String getDescribe() {
+        return describe;
+    }
+
+    public void setDescribe(String describe) {
+        this.describe = describe;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
